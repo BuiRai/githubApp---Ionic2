61f98931632ab33d1db023d2cfc48c428ebc59f4
crhistian.uc <practica1.sistemas@galletasdonde.com>
2017-01-04 16:39:27 -0600

    Add three pages and User model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
-import { ListPage } from '../pages/list/list';
+
+import { UsersPage } from './../pages/users/users';
+import { ReposPage } from './../pages/repos/repos';
+import { OrganisationsPage } from './../pages/organisations/organisations';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+	declarations: [
+		MyApp,
+		UsersPage,
+		ReposPage,
+		OrganisationsPage
+	],
+	imports: [
+		IonicModule.forRoot(MyApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		UsersPage,
+		ReposPage,
+		OrganisationsPage
+	],
+	providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
