6d739067d4f87f9b1d390263ad6d1c72e410936e
buirai <buirai_naetle@hotmail.com>
2017-01-05 15:36:48 -0600

    User details finished

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { UserDetailsPage } from './../../pages/user-details/user-details';
+
 import { User } from '../../models/user';
 
 import {  GithubUsers } from '../../providers/github-users';
@@ -17,15 +19,38 @@ import {  GithubUsers } from '../../providers/github-users';
 })
 export class UsersPage {
 
-	users: User[]
+	users: User[];
+	originalUsers: User[];
 
 	constructor(public navCtrl: NavController, private githubUsers: GithubUsers) {
 		githubUsers.load().subscribe( users => {
 			this.users = users;
+			this.originalUsers = users;
 		});
 	}
 
 	ionViewDidLoad() {
 		console.log('Hello Users Page');
+	}
+
+	goToDetails(login: string) {
+		this.navCtrl.push(UserDetailsPage, {login});
+	}
+
+	search(searchEvent) {
+		let term = searchEvent.target.value;
+		// We will only perform the search if we have 3 or more characters
+
+		if (term.trim() === '' || term.trim().length < 3) {
+			// Load cached users
+			this.users = this.originalUsers;
+		} else {
+			// Get the searched users from github
+			console.log("Tesrm:", term);
+			this.githubUsers.searchUsers(term).subscribe(data => {
+				this.users = data['items'];
+				
+			});
+		}
 	}
 }
